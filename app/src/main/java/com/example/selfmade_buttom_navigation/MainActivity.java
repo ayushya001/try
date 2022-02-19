02bd1eb828c58c3619249c22221e368ac9e6a631
ayushya001 <ayushbravo001@gmail.com>
2022-02-19 07:02:32 +0530

    first try to add project on github

---
+++ app/src/main/java/com/example/selfmade_buttom_navigation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.selfmade_buttom_navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
